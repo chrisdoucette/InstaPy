dd029f94b4d5a6353a3a26ca4dd95aefa71b51e6
Chris Doucette <chris.doucette16@student.bridgew.edu>
2020-04-13 13:12:56 -0400

    util tests

---
+++ tests/test_utils.py
@@ -0,0 +1,25 @@
+from instapy import time_util
+from instapy import util
+import pytest
+from datetime import datetime
+
+def test_get_time():
+    labels = ["this_hour","this_minute","today"]
+    results = time_util.get_time(labels)
+    date = datetime.now()
+    assert results == [date.strftime("%H"), date.strftime("%M"), date.strftime("%Y-%m-%d")]
+
+def test_username_url_to_username():
+    url = "https://www.instagram.com/test_username/rest_of_url"
+    result = util.username_url_to_username(url)
+    assert result == "test_username"
+
+def test_format_number():
+    number = "3.2k"
+    assert util.format_number(number) == 3200
+
+def test_deform_emojis():
+    emoji = "🤡 wow!"
+    new_text, emojiless_text = util.deform_emojis(emoji)
+    assert new_text == " (clown face) wow!"
+    assert emojiless_text == " wow!"
